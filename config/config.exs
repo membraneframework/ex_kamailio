2051abe63b8bb7371ebd2ccad36587f2af004d51
Javi Gallart <jgallart@sonoc.io>
2025-10-21 15:59:59 +0000

    update membrane_pipeline config

--- config/config.exs
+++ config/config.exs
@@ -7,3 +7,8 @@ config :ex_media,
   media_ip: System.get_env("MEDIA_IP", "192.168.36.76"),
   # Inclusive port range (will allocate even base for RTP, odd for RTCP)
   port_range: 11000..40000
+
+config :shine_membrane_pipeline,
+  streams_in_batch: System.get_env("MEMBRANE_STREAMS_IN_BATCH", "8") |> String.to_integer(),
+  tick_time_ms: System.get_env("MEMBRANE_TICK_TIME_MS", "10") |> String.to_integer(),
+  latency_tick_count: System.get_env("MEMBRANE_LATENCY_TICK_COUNT", "6") |> String.to_integer()
